76e8a7422102d423178d392548806aeb220ee3a4
Sandmax-1 <m.sandman@hotmail.co.uk>
2022-04-06 16:26:05 +0100

    new version of runner

---
+++ new_vision_runner.py
@@ -0,0 +1,57 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+
+def predict(project_id, model_id, file_path):
+    """Predict."""
+    # [START automl_vision_classification_predict]
+    from google.cloud import automl
+
+    # TODO(developer): Uncomment and set the following variables
+    # project_id = "YOUR_PROJECT_ID"
+    # model_id = "YOUR_MODEL_ID"
+    # file_path = "path_to_local_file.jpg"
+    
+    prediction_client = automl.PredictionServiceClient()
+
+    # Get the full path of the model.
+    model_full_id = automl.AutoMlClient.model_path(project_id, "europe-west4", model_id)
+
+    # Read the file.
+    with open(file_path, "rb") as content_file:
+        content = content_file.read()
+
+    image = automl.Image(image_bytes=content)
+    payload = automl.ExamplePayload(image=image)
+
+    # params is additional domain-specific parameters.
+    # score_threshold is used to filter the result
+    # https://cloud.google.com/automl/docs/reference/rpc/google.cloud.automl.v1#predictrequest
+    params = {"score_threshold": "0.8"}
+
+    request = automl.PredictRequest(name=model_full_id, payload=payload, params=params)
+    response = prediction_client.predict(request=request)
+
+    print("Prediction results:")
+    for result in response.payload:
+        print("Predicted class name: {}".format(result.display_name))
+        print("Predicted class score: {}".format(result.classification.score))
+    # [END automl_vision_classification_predict]
+
+
+if __name__ == '__main__':
+    file_path = os.environ['FILE_PATH'] #sys.argv[1]
+    project_id = os.environ['PROJECT_ID'] #sys.argv[2]
+    model_id = os.environ['MODEL_ID'] #sys.argv[3]  
+    print(predict(file_path, project_id, model_id))
