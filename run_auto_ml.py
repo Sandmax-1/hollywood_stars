b5d09fa273d1e75947d04fc6287bbabbfa249f0f
Sandmax-1 <m.sandman@hotmail.co.uk>
2022-04-06 15:52:40 +0100

    ksdhjadh

--- run_auto_ml.py
+++ run_auto_ml.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from google.cloud import automl_v1beta1
 from google.cloud.automl_v1beta1.proto import service_pb2
@@ -14,9 +15,9 @@ def get_prediction(content, project_id, model_id):
     return request  # waits till request is returned
 
 if __name__ == '__main__':
-    file_path = sys.argv[1]
-    project_id = sys.argv[2]
-    model_id = sys.argv[3]  
+    file_path = os.environ['FILE_PATH'] #sys.argv[1]
+    project_id = os.environ['PROJECT_ID'] #sys.argv[2]
+    model_id = os.environ['MODEL_ID'] #sys.argv[3]  
     with open(file_path, 'rb') as ff:
       content = ff.read()   
     print get_prediction(content, project_id, model_id)
